22a6b70d5e944590fc156e0137044640390ff06e
Nathan Hedge <23344786+10Nates@users.noreply.github.com>
2024-03-05 20:55:30 -0600

    improved bypass rate, added README, cleaned up CLI

---
+++ README.md
@@ -0,0 +1,34 @@
+# Humanlike AI Chat
+
+Humanlike AI Chat is aterminal-based LLM UI designed to study how to bypass AI text detection.
+
+**It is not perfect,** and in fact it isn't even especially great (it fails most of the time), but it has a much higher success rate than a normal responses at bypassing AI text detection due to its background/lore generation and its ability for a much higher temperature due to its very long system prompt. That being said, its responses are far more human *feeling,* which is why I named it as such.
+
+## Installation
+
+- Install Python 3
+- Install [Ollama](https://ollama.com)
+- Install `dolphin-mixtral:latest` (default) or whatever model you plan to use with `ollama pull [model]`
+- Install Python dependencies with `pip install ollama termcolor`
+
+## Usage
+
+- Run the script with `python main.py` or use `python main.py --model [model]` to set a different model
+- Type your question and press enter. For the purposes of the tool, you are Olivia and the AI is named Alex.
+- You can enter `/prompt` for the last system prompt, `/exit` to leave
+  - Note on `/prompt`- this is the system prompt that is generated in the first stage of the prompting process. It is regenerated for every prompt and cannot be controlled otherwise. The purpose of this system prompt is to provide "character" to the model that allows it to handle temperature and generate more cohesive responses that align with how a person would respond.
+
+## Disclaimer
+
+This is for research purposes only. I do not condone using this to bypass AI detection in any setting - especially schools. Education is important and AI does not help you learn. Stop kidding yourself. You have to struggle to learn and grow; otherwise, you aren't learning.
+
+## Example
+
+**Prompt:** Hamburgers or hotdogs?
+
+**Default Response:** I can't eat, but both hamburgers and hotdogs are popular foods. Hamburgers consist of a ground beef patty, topped with various condiments such as lettuce, tomatoes, cheese, onions, mustard, and ketchup, typically served between two slices of bread or a bun. Hotdogs, on the other hand, are made from finely chopped meat in casings which is then boiled or grilled, and served in a hotdog bun with various toppings like mustard, ketchup, relish, onions, or chili.
+
+**"Alex's" Response:** 🤔 Hotdogs any day! They're portable, versatile, and infinitely customizable to one’s taste preferences - which makes them an ideal choice for picnics, ball games... or even a quick bite between classes. I mean, you can toss it on the grill in its bun without breaking a sweat while juggling a dozen other things! What's not to love?
+
+[![default response score](./README_src/undetectable_ai_1.png)](https://undetectable.ai/)
+[![Alex's response score](./README_src/undetectable_ai_2.png)](https://undetectable.ai/)
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import ollama
+import sys
 from termcolor import colored as c
 
+# constants
 model = 'dolphin-mixtral:latest'
+if "--model" in sys.argv:
+    model = sys.argv[sys.argv.index("--model") + 1]
 template = """
 Olivia: {question}
 Alex: """
-prompt_prompter = """Write detailed lore for a character named Alex, a professor at MIT who was just been asked the following question: "{question}"
+
+# "Lore" works better. Probably tagging into DnD characters or something.
+prompt_prompter = """Write detailed lore for a character named Alex, an OPINIONED professor at MIT who was just been asked the following question: "{question}"
 
 Do not answer the question, rather write a background story and upbringing that led to Alex being an expert in the field that said question involves."""
 
+#globals
 system = ""
 chat_history = ""
-#with open("system.txt", "r") as file:
-#    system += file.read()
 
 def gen_system_prompt(question):
     global system
@@ -21,23 +26,22 @@ def gen_system_prompt(question):
         raw=False, # standard question
         prompt=prompt_prompter.format(question=question)
     )
-    system = "About Alex: " + response['response']
-    system += "\n\nYou are Alex, having a conversation with Olivia, a colleague.\n"
+    system = response['response']
+    system += "\n\n-- The following is a conversation between Alex and Olivia, a colleague and close friend of Alex who have known each other since high school. --\n\n"
     
-    
-
 def gen_next(question):
     global chat_history
     chat_history += template.format(question=question)
     response = ollama.generate(
         model=model,
-        raw=False,
-        system=system,
-        prompt=chat_history,
+        raw=True, # using the model template binds it to being more robotic
+        #system=system,
+        prompt=system+chat_history,
         options={
             'temperature': 3, 
-            # system prompt is long enough that a very high temperature is tolerable
+            # system prompt is long enough that a very high temperature is tolerable.
             # without the long prompt this temperature quickly devolves into complete gibberish.
+            'repeat_penalty': 1.2,
             'stop': ['<EOT>', 'Alex:', 'Olivia:'],
         }
     )
@@ -49,16 +53,23 @@ def main():
     while (True):
         # input
         question = input(c("You/Olivia ('/prompt' for last system prompt, '/exit' to leave): ", "green"))
+
+        #commands
         if (question == '/exit'):
             break
         elif (question == "/prompt"):
             print(f"Last system prompt: {system}")
-        else:
-            gen_system_prompt(question)
-            response = gen_next(question)
-            print(c("Bot/Alex: ", "light_blue") + response)
+            continue
+        elif (question.startswith("/")):
+            print(c("Unknown command", "red"))
+            continue
+
+        gen_system_prompt(question)
+        response = gen_next(question)
+        print(c("Bot/Alex: ", "light_blue") + response)
     
     print(c("Bot/Alex: ", "light_blue") + "Goodbye!") # give it a little character
 
 if __name__ == "__main__":
+    print(c("Using " + model + ". You can change the model with --model [model]", "red"))
     main()
